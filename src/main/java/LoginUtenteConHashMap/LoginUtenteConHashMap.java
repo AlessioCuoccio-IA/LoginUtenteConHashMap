dadca619469419f1c23b9df3ae539351bd224a02
AlessioCuoccio-IA <acuoccio@studenti.apuliadigitalmaker.it>
2021-09-25 10:34:16 +0200

    Initial commit

---
+++ src/main/java/LoginUtenteConHashMap/LoginUtenteConHashMap.java
@@ -0,0 +1,13 @@
+package LoginUtenteConHashMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginUtenteConHashMap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginUtenteConHashMap.class, args);
+    }
+
+}
---
+++ src/test/java/LoginUtenteConHashMap/Esercitazione1ApplicationTests.java
@@ -0,0 +1,13 @@
+package LoginUtenteConHashMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercitazione1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
